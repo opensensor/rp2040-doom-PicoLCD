3e570c79ce6bb6223d363f99f64b85305c4879e7
bob <rsheldiii@gmail.com>
2022-12-20 15:35:07 +0000

    switch to same wiring harness and also realize I never loaded the wad :facepalm:

--- src/pico/pico-screens/screens/ssd1306_70_40.cpp
+++ src/pico/pico-screens/screens/ssd1306_70_40.cpp
@@ -1,13 +1,21 @@
 #include "ssd1306_70_40.hpp"
 #include "pico/ssd1306.h"
 
+    // .spi      = PICO_DEFAULT_SPI_INSTANCE,
+    // .gpio_din = PICO_DEFAULT_SPI_TX_PIN,
+    // .gpio_clk = PICO_DEFAULT_SPI_SCK_PIN,
+    // .gpio_cs  = PICO_DEFAULT_SPI_CSN_PIN,
+    // .gpio_dc  = 20,
+    // .gpio_rst = 21,
+    // .gpio_bl  = 22,
+
 extern "C" {
     void* ssd1306_70_40_initScreen(void) {
-        SSD1306* display = new SSD1306(128, 64, spi0, /*baudrate*/ 8000 * 1000, /*mosi*/ 3, /*cs*/ 5, /*sclk*/ 2, /*reset*/ 6, /*dc*/ 7);
+        SSD1306* display = new SSD1306(128, 64, PICO_DEFAULT_SPI_INSTANCE, /*baudrate*/ 8000 * 1000, /*mosi*/ PICO_DEFAULT_SPI_TX_PIN, /*cs*/ PICO_DEFAULT_SPI_CSN_PIN, /*sclk*/ PICO_DEFAULT_SPI_SCK_PIN, /*reset*/ 21, /*dc*/ 20);
         display->init();    
 
         for(int y = 24; y < 64; y++) {
-            for(int x = 28; x < 98; x++) {
+            for(int x = 28; x < 98; x+=3) {
                 display->draw_pixel(x, y, SSD1306_COLOR_ON);        
             }
         }
